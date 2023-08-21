15c4921b05699c403f5dbd9121a1b499fcb3b306
Erik Norfelt <erik.norfelt@monitor.se>
2023-08-21 08:38:39 +0200

    initial

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+
+AsyncWebServer server(80);
+
+void setup()
+{
+  Serial.begin(115200);
+
+  WiFi.begin("LMV19", "NorfWiks");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to WiFi " + WiFi.localIP().toString());
+
+  server.serveStatic("/", SPIFFS, "/www/")
+      .setDefaultFile("index.html")
+      .setCacheControl("max-age=600");
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(200, "text/plain", "Hello, world"); });
+
+  server.begin();
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+}
