91055d5b09011f6ff1377e7247606fe09a211b57
Erik Norfelt <erik.norfelt@monitor.se>
2024-05-31 15:09:39 +0200

    basic server state handling

---
+++ lib/core/scoreBoardState.h
@@ -0,0 +1,11 @@
+#ifndef SCOREBOARDSTATE_H
+#define SCOREBOARDSTATE_H
+
+struct ScoreBoardState {
+  int home;
+  int away;
+  int inning;
+  int outsInInning;
+};
+
+#endif // SCOREBOARDSTATE_H
--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,30 @@
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+#include <AsyncJson.h>
+#include <ArduinoJson.h>
 #include <LittleFS.h>
 #include <FS.h>
+#include <ScoreBoardState.h>
 
-AsyncWebServer server(5000);
+AsyncWebServer server(80);
+
+ScoreBoardState scoreBoardState = {.home = 0, .away = 0, .inning = 1, .outsInInning = 0};
+
+AsyncCallbackJsonWebHandler *updateHandler = new AsyncCallbackJsonWebHandler("/api/score-board/update", [](AsyncWebServerRequest *request, JsonVariant &json)
+{
+  JsonObject bodyObj = json.as<JsonObject>();
+  JsonObject payloadObj = bodyObj["payload"].as<JsonObject>();
+  scoreBoardState.home = payloadObj["home"];
+  scoreBoardState.away = payloadObj["away"];
+  scoreBoardState.inning = payloadObj["inning"];
+  scoreBoardState.outsInInning = payloadObj["outsInInning"];
+
+  String response;
+  serializeJson(payloadObj, response);
+  Serial.println("Received update: " + response);
+  request->send(200, "application/json", response); 
+});
 
 void setup()
 {
@@ -13,7 +33,7 @@ void setup()
 
   Serial.println("HTTP server starting..");
 
-  WiFi.begin("","");
+  WiFi.begin("", "");
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(1000);
@@ -27,18 +47,30 @@ void setup()
     return;
   }
 
-  server.serveStatic("/", LittleFS, "/browser")
+  server.serveStatic("/", LittleFS, "/browser/")
       .setDefaultFile("index.html")
       .setCacheControl("max-age=600");
 
-  server.on("/hello", HTTP_GET, [](AsyncWebServerRequest *request) {
-    Serial.println("Hello World");
-    request->send(200, "text/plain", "Hello World!");
+  server.addHandler(updateHandler);
+
+  server.on("/api/score-board/load", HTTP_GET, [](AsyncWebServerRequest *request) 
+  {
+    JsonDocument doc;
+    doc["home"] = scoreBoardState.home;
+    doc["away"] = scoreBoardState.away;
+    doc["inning"] = scoreBoardState.inning;
+    doc["outsInInning"] = scoreBoardState.outsInInning;
+
+    String response;
+    serializeJson(doc, response);
+    Serial.println("Sending load: " + response);
+    request->send(200, "application/json", response); 
   });
 
-  server.onNotFound([](AsyncWebServerRequest *request) {
+  server.onNotFound([](AsyncWebServerRequest *request) 
+  {
     Serial.printf("Not found: %s\n", request->url().c_str());
-    request->send(404, "text/plain", "Not found");
+    request->send(404, "text/plain", "Not found"); 
   });
 
   server.begin();
