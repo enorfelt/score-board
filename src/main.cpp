8a2dd1ed5b46d432c9b80cbf4c9bb54af15e9afe
Erik Norfelt <erik.norfelt@monitor.se>
2024-07-08 22:09:55 +0200

    wifi manager

--- src/main.cpp
+++ src/main.cpp
@@ -1,90 +1,49 @@
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <ESPAsyncTCP.h>
-#include <ESPAsyncWebServer.h>
-#include <AsyncJson.h>
-#include <ArduinoJson.h>
+#include <ESP8266WiFi.h> 
 #include <LittleFS.h>
 #include <FS.h>
-#include <ScoreBoardState.h>
-#include <ScoreBoardCom.h>
+#include <ScoreBoardServer.h>
+#include <EspAsyncWiFiManager.h>
 
 AsyncWebServer server(80);
-
-ScoreBoardState scoreBoardState = {.home = 0, .away = 0, .inning = 1, .outsInInning = 0};
-ScoreBoardCom compcomm(12, 14);
-
-const char *MESSAGE_PASS = "Ok";
-
-struct UpdateResult
-{
-  bool success;
-  String message;
-};
-
-AsyncCallbackJsonWebHandler *updateHandler = new AsyncCallbackJsonWebHandler("/api/score-board/update", [](AsyncWebServerRequest *request, JsonVariant &json)
-                                                                             {
-  JsonObject bodyObj = json.as<JsonObject>();
-  JsonObject payloadObj = bodyObj["payload"].as<JsonObject>();
-  scoreBoardState.home = payloadObj["home"];
-  scoreBoardState.away = payloadObj["away"];
-  scoreBoardState.inning = payloadObj["inning"];
-  scoreBoardState.outsInInning = payloadObj["outsInInning"];
-
-  String response;
-  serializeJson(payloadObj, response);
-  Serial.println("Received update: " + response);
-  request->send(200, "application/json", response); });
+DNSServer dns;
 
 void setup()
 {
+  WiFi.mode(WIFI_STA);
+
   Serial.begin(115200);
 
-  Serial.println("HTTP server starting..");
+  Serial.println("\n Starting");
 
-  WiFi.begin("", "");
-  while (WiFi.status() != WL_CONNECTED)
+  AsyncWiFiManager wm(&server, &dns);
+
+  bool res;
+
+  res = wm.autoConnect("ScoreBoard Config");
+  if (!res)
   {
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
+    Serial.println("Failed to connect to WiFi");
+    delay(3000);
+    ESP.reset();
   }
-  Serial.println("Connected to WiFi " + WiFi.localIP().toString());
-
-  if (!LittleFS.begin())
+  else
   {
-    Serial.println("An Error has occurred while mounting LittleFS");
-    return;
+    Serial.println("Connected to WiFi " + WiFi.localIP().toString());
+
+    if (!LittleFS.begin())
+    {
+      Serial.println("An Error has occurred while mounting LittleFS");
+      return;
+    }
+
+    Serial.println("Staring HTTP server...");
+
+    ScoreBoardServer scoreBoardServer(&server);
+    scoreBoardServer.Start();
+
+    Serial.println("HTTP server started");
   }
-
-  server.serveStatic("/", LittleFS, "/browser/")
-      .setDefaultFile("index.html")
-      .setCacheControl("max-age=600");
-
-  server.addHandler(updateHandler);
-
-  server.on("/api/score-board/load", HTTP_GET, [](AsyncWebServerRequest *request)
-            {
-    JsonDocument doc;
-    doc["home"] = scoreBoardState.home;
-    doc["away"] = scoreBoardState.away;
-    doc["inning"] = scoreBoardState.inning;
-    doc["outsInInning"] = scoreBoardState.outsInInning;
-
-    String response;
-    serializeJson(doc, response);
-    Serial.println("Sending load: " + response);
-    request->send(200, "application/json", response); });
-
-  server.onNotFound([](AsyncWebServerRequest *request)
-                    {
-    Serial.printf("Not found: %s\n", request->url().c_str());
-    request->send(404, "text/plain", "Not found"); });
-
-  server.begin();
-
-  compcomm.Open();
-
-  Serial.println("HTTP server started");
 }
 
 void loop()
@@ -92,95 +51,3 @@ void loop()
   // put your main code here, to run repeatedly:
 }
 
-UpdateResult updateAll()
-{
-  UpdateResult result;
-  result.success = true;
-  String s;
-  int i;
-
-  // Home
-  i = scoreBoardState.home / 10;
-  s = "wd 0 " + String(i);
-  if (!compcomm.SendCommandLookForString(s, MESSAGE_PASS))
-  {
-    result.success = false;
-    result.message = "Unexpected answer from unit. Command was " + s + ". Was looking for string " + MESSAGE_PASS + " in output " + compcomm.fetchedOutputString;
-    return result;
-  }
-
-  s = "wd 1 " + String(scoreBoardState.home - (10 * i));
-  if (!compcomm.SendCommandLookForString(s, MESSAGE_PASS))
-  {
-    result.success = false;
-    result.message = "Unexpected answer from unit. Command was " + s + ". Was looking for string " + MESSAGE_PASS + " in output " + compcomm.fetchedOutputString;
-    return result;
-  }
-
-  // Innings
-  s = "wd 2 " + String(scoreBoardState.inning);
-  if (!compcomm.SendCommandLookForString(s, MESSAGE_PASS))
-  {
-    result.success = false;
-    result.message = "Unexpected answer from unit. Command was " + s + ". Was looking for string " + MESSAGE_PASS + " in output " + compcomm.fetchedOutputString;
-    return result;
-  }
-
-  // Guest
-  i = scoreBoardState.away / 10;
-  s = "wd 3 " + String(i);
-  if (!compcomm.SendCommandLookForString(s, MESSAGE_PASS))
-  {
-    result.success = false;
-    result.message = "Unexpected answer from unit. Command was " + s + ". Was looking for string " + MESSAGE_PASS + " in output " + compcomm.fetchedOutputString;
-    return result;
-  }
-
-  s = "wd 4 " + String(scoreBoardState.away - (10 * i));
-  if (!compcomm.SendCommandLookForString(s, MESSAGE_PASS))
-  {
-    result.success = false;
-    result.message = "Unexpected answer from unit. Command was " + s + ". Was looking for string " + MESSAGE_PASS + " in output " + compcomm.fetchedOutputString;
-    return result;
-  }
-
-  // Out
-  if (( scoreBoardState.outsInInning > 0 && scoreBoardState.outsInInning < 3 ) || scoreBoardState.outsInInning > 3)
-  {
-    if (!compcomm.SendCommandLookForString("wb 5 0 1", MESSAGE_PASS))
-    {
-      result.success = false;
-      result.message = "Unexpected answer from unit. Command was " + s + ". Was looking for string " + MESSAGE_PASS + " in output " + compcomm.fetchedOutputString;
-      return result;
-    }
-  }
-  else
-  {
-    if (!compcomm.SendCommandLookForString("wb 5 0 0", MESSAGE_PASS))
-    {
-      result.success = false;
-      result.message = "Unexpected answer from unit. Command was " + s + ". Was looking for string " + MESSAGE_PASS + " in output " + compcomm.fetchedOutputString;
-      return result;
-    }
-  }
-
-  if (( scoreBoardState.outsInInning > 1 && scoreBoardState.outsInInning < 3 ) || scoreBoardState.outsInInning > 4)
-  {
-    if (!compcomm.SendCommandLookForString("wb 5 1 1", MESSAGE_PASS))
-    {
-      result.success = false;
-      result.message = "Unexpected answer from unit. Command was " + s + ". Was looking for string " + MESSAGE_PASS + " in output " + compcomm.fetchedOutputString;
-      return result;
-    }
-  }
-  else
-  {
-    if (!compcomm.SendCommandLookForString("wb 5 1 0", MESSAGE_PASS))
-    {
-      result.success = false;
-      result.message = "Unexpected answer from unit. Command was " + s + ". Was looking for string " + MESSAGE_PASS + " in output " + compcomm.fetchedOutputString;
-      return result;
-    }
-  }
-  return result;
-}
